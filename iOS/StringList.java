a4850f780c086744df5bf8586c517fdd1fa3ed39
damar <damar@gopaktor.com>
2016-11-25 08:51:59 +0700

    add more id and list

--- iOS/StringList.java
+++ iOS/StringList.java
@@ -7,17 +7,33 @@ public class StringList {
 	public String SlandingStatic3	= "If someone you've liked happens to like you as well...";
 	public String SlandingStatic4	= "Chat with your matches inside the app.";
 	public String SFacebookLogin	= "FACEBOOK LOGIN earn 250 free points";
-	public String SSignupPhone		= "Sign up with phone number";
+	public String SSignupPhone	= "Sign up with phone number";
 	
 	//enter phone number
-	public String SLoginPhonePageHeader		="Log Into Paktor";
-	public String SLoginPhonePageTitle		="Enter your phone number";
-	public String SLoginPhonePageContent	="Tap Next to get an SMS confirmation from Account Kit by Facebook to help you use Paktor. You don't need a Facebook account to use it. Message and data rates may apply. Learn how Facebook uses your info.";
-	public String SLoginPhonePageButton   	="Next"; 
+	public String SLoginPhonePageHeader	= "Log Into Paktor";
+	public String SLoginPhonePageTitle	= "Enter your phone number";
+	public String SLoginPhonePageContent	= "Tap Next to get an SMS confirmation from Account Kit by Facebook to help you use Paktor. You don't need a Facebook account to use it. Message and data rates may apply. Learn how Facebook uses your info.";
+	public String SLoginPhonePageButton	= "Next"; 
 	
 	//enter wrong phone
-	public String SEnterInvPhoneNumber	="Please enter a valid phone number.";
-	public String SEnterTryAgainButton	="Try Again";
-	public String SEnterCancelButton	="Cancel";
-	//""
+	public String SEnterInvPhoneNumber	= "Please enter a valid phone number.";
+	public String SEnterTryAgainButton	= "Try Again";
+	public String SEnterCancelButton	= "Cancel";
+	
+	//home page tab
+	public String SHomeTab	= "home";
+	public String SInterestTab	= "interest";
+	public String SProfileTab	= "profile";
+	public String SConnect	= "connect";
+	
+	public String SHomeHeader	= "Home";
+	public String SInterestHeader	= "Interest";
+	public String SProfileHeader	= "Profile";
+	public String SConnectHeader	= "Connect";
+	
+	
+	public String SInterestCampaign	="Want to know who has showed interest in you? Go Premium now!";
+	public String SInterestPremium	="GO PREMIUM";
+	
+	
 }
