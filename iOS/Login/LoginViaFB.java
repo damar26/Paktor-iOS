f82fbc881a00f1006b53bba463f8f4fdba4b4b97
damar <damar@gopaktor.com>
2016-11-25 13:51:27 +0700

    add credential

--- iOS/Login/LoginViaFB.java
+++ iOS/Login/LoginViaFB.java
@@ -1,12 +1,14 @@
 package iOS.Login;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -17,12 +19,8 @@ import iOS.StringList;
 
 
 public class LoginViaFB{
-	//Initiate driver
 	public WebDriver driver;
-	//private AppiumDriver driver;
-	//private AppiumDriver driver;
 	
-	//public MobileDriver driver;
 	IdList	id 	= new IdList();
 	ReUseAction action 	= new ReUseAction();
 	StringList string	= new StringList();
@@ -34,28 +32,33 @@ public class LoginViaFB{
 		capabilities.setCapability("platformName", "iOS");
 		capabilities.setCapability("platformVersion", "10.1");
 		capabilities.setCapability("app", "/Users/damarananta/Library/Developer/Xcode/DerivedData/Paktor-blrrravdhtbsevcojsalzwjsfjhe/Build/Products/Debug-iphonesimulator/Paktor.app");
+		
 		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		System.out.println("Setting is OK");
 	}
 
-	
 	//Test landing page
 	@Test (enabled=true, priority=1)
 	public void loginFB() throws InterruptedException {
+		action.Wait();
+		//action.Wait();
 		System.out.println("masuk");
-		action.LongWait();
-		WebElement loginFB= driver.findElement(id.LoginFacebook);
+		WebElement loginFB = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(id.LoginFacebook));
+		//WebElement loginFB= driver.findElement(id.LoginFacebook);
 		//WebElement login = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(id.LoginFacebook));
 		loginFB.click();
-		action.wait();
-		//(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("Email atau Telepon"))).sendKeys("ll");;
-		//driver.findElement(By.name(name)("Email atau Telepon")).sendKeys("trial");
+		action.Wait();
+		System.out.println("MasukFB");
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("Email atau Telepon"))).sendKeys("dear.ananta@gmail.com");
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("Kata Sandi Facebook"))).sendKeys("dear.ananta@gmail.com");
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("Masuk"))).click();
+		action.Wait();
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("OK"))).click();  //oppposite: Cancel
+		action.Wait();
+		//(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("Cancel"))).click();
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.name("Ask me later"))).click();
 	}	
 	
-	@AfterMethod
-	public void delay(){
-
-	}
 	@AfterClass
 	public void tearDown() throws Exception {
 		action.LongWait();
